36bb5f31d371ceb95ac54b3d1dc0bb3ecc7d9e6b
Divasdev <sharmadivas881@gmail.com>
2025-11-05 16:58:16 +0530

    counterproblem

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,44 @@
 
-let fullName= 'Divas Sharma';
+/*let fullName= 'Divas Sharma';
 let favoriteCar="Porsche";
 
 document.getElementById("paragraph1").textContent= `Your Name is :${fullName}`;
 document.getElementById("paragraph2").textContent= `Your favorite car is :${favoriteCar}`;
+
+
+let userName;
+ userName=prompt("what is your username??")
+console.log(userName);
+
+document.getElementById("my-button").onclick= 
+function(){
+   username=document.getElementById('textbox').value;
+   document.getElementById("header").textContent=`Hello:${username}`
+   console.log(username);
+} ;*/
+
+const decreaseBtn=document.getElementById("btn-dec");
+const resetBtn=document.getElementById("btn-reset");
+const increaseBtn=document.getElementById("btn-inc");
+const  counterNum=document.getElementById("counter");
+let count=0;
+
+increaseBtn.onclick =function(){
+   count++;
+   counterNum.textContent=count;
+}
+
+decreaseBtn.onclick=function(){
+   count--
+   counterNum.textContent= count;
+}
+
+resetBtn.onclick=function(){
+   count=0
+   counterNum.textContent= count;
+}
+
+
+
+
+

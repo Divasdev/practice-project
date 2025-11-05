52690043c1df5e6e604e20969d40d07e75002549
Divasdev <sharmadivas881@gmail.com>
2025-11-05 11:59:48 +0530

    javascript comeback

---
+++ javascript/scripts.js
@@ -0,0 +1,6 @@
+
+let fullName= 'Divas Sharma';
+let favoriteCar="Porsche";
+
+document.getElementById("paragraph1").textContent= `Your Name is :${fullName}`;
+document.getElementById("paragraph2").textContent= `Your favorite car is :${favoriteCar}`;

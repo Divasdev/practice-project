749ce308469d390298b0fbc64055706dcd62d60e
Divasdev <sharmadivas881@gmail.com>
2025-11-20 10:07:01 +0530

    KEY EVENTS IN JAVASCRIPTS

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -941,5 +941,53 @@ mybox.addEventListener("mouseout" ,event=>{
 
 
 
+const mybox = document.getElementById("log-btn");
+const moveAmount = 10;
+
+let x = 0;
+let y = 0;
+
+document.addEventListener("keydown", event => {
+  if (event.key.startsWith("Arrow")) {
+
+   event.preventDefault();
+    switch (event.key) {
+      case "ArrowUp":
+        y -= moveAmount;
+        break;
+      case "ArrowDown":
+        y += moveAmount;
+        break;
+      case "ArrowLeft":
+        x -= moveAmount;
+        break;
+      case "ArrowRight":
+        x += moveAmount;
+        break;
+    }
+
+    mybox.style.top  = `${y}px`;
+    mybox.style.left = `${x}px`;
+  }
+});
+
+
+
+document.addEventListener("keydown",event=>{
+   mybox.textContent="Apple";
+   mybox.style.backgroundColor="tomato";
+});
+
+
+document.addEventListener("keyup",event=>{
+mybox.textContent="Google";
+mybox.backgroundColor="lightblue";
+
+});
+
+
+
+
+
 
 

b03ff9dcb6bdd1f262f02868055c56d3e7e7b8d3
Divasdev <sharmadivas881@gmail.com>
2025-11-21 08:39:17 +0530

    IMAGE SLIDER IN JAVASCRIPT

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1025,55 +1025,48 @@ buttons.forEach(button => {
 
 */
 
-const slides =document.querySelectorAll(".slides img");
-let slideIndex=0;
-let intervalId =null;
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
 
-document.addEventListener("DOMContentLoaded",intializeSlider);
-function intializeSlider(){
+intializeSlider();
 
-   if (slides.length >0){
-
-slides[slideIndex].classList.add("displaySlide");
-  intervalId=setInterval(nextSlide,3000)
+function intializeSlider() {
+  if (slides.length > 0) {
+    slides[slideIndex].classList.add("displaySlide");
+    intervalId = setInterval(nextSlide, 6000);
+  }
 }
 
-   }
-  
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
 
-function showSlide(index){
+  slides.forEach(slide => {
+    slide.classList.remove("displaySlide");
+  });
 
-   if (index>= slides.length){
-        slideIndex=0;
-
-   }
-   else if (index<0){
-      slideIndex=slides.length-1;
-
-   }
-
-
-
-
-slides.forEach( slide =>{
-   slide.classList.remove("displaySlide");
-
-})
-slides[slideIndex].classList.add("displaySlide");
+  slides[slideIndex].classList.add("displaySlide");
 }
-function prevSlide(){
-   clearInterval(intervalId);
+
+function prevSlide() {
+  clearInterval(intervalId);
   slideIndex--;
   showSlide(slideIndex);
-
-
-
-
 }
-function nextSlide(){
-   slideIndex++;
-   showSlide(slideIndex);
+
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
 }
+
+// Expose functions globally for HTML onclick handlers
+window.prevSlide = prevSlide;
+window.nextSlide = nextSlide;
+
 
 
 
